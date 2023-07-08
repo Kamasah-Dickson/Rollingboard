b55bf82cee7c13524eb8b9bb0bfdd9b6b46845af
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-08 23:21:22 +0100

    FIX: redirect to main page if user is logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import About from "../components/About";
+import { useEffect, useState } from "react";
+import { auth } from "../configs/firebase";
+import Link from "next/link";
 
 export default function Home() {
+	const [currentUser, setCurrentUser] = useState<null | any>(null);
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			setCurrentUser(user);
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+
 	return (
 		<>
 			<section className="hero mt-12 flex select-none flex-col items-center justify-center p-1">
@@ -14,12 +31,13 @@ export default function Home() {
 					Create, share, and get feedback with collaborative boards for rapid
 					development.
 				</p>
-				<button
+				<Link
+					href={currentUser ? "/rollingboard" : "/login"}
 					type="button"
 					className=" text-md mb-5 mt-5 cursor-pointer rounded-md bg-[#610386] px-6 py-2 font-medium text-white hover:bg-[#8010adec] active:scale-[1.02]"
 				>
 					Create Kanban Board
-				</button>
+				</Link>
 			</section>
 			<About />
 		</>
