146f8d092732bb0c1e857d58d9cffecfa1693817
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-15 11:06:35 +0000

    add client side createButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,8 @@
-"use client";
-
 import About from "../components/About";
-import { useEffect, useState } from "react";
-import { auth } from "../configs/firebase";
-import Link from "next/link";
 import Footer from "@/components/Footer";
+import CreateBoardButton from "@/components/CreateBoardButton";
 
 export default function Home() {
-	const [currentUser, setCurrentUser] = useState<null | any>(null);
-
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged((user) => {
-			setCurrentUser(user);
-		});
-
-		return () => {
-			unsubscribe();
-		};
-	}, []);
-
 	return (
 		<>
 			<section className="hero mt-12 flex select-none flex-col items-center justify-center p-1">
@@ -32,13 +16,7 @@ export default function Home() {
 					Create, share, and get feedback with collaborative boards for rapid
 					development.
 				</p>
-				<Link
-					href={currentUser ? "/rollingboard" : "/login"}
-					type="button"
-					className=" text-md mb-5 mt-5 cursor-pointer rounded-md bg-[#610386] px-6 py-2 font-medium text-white hover:bg-[#8010adec] active:scale-[1.02]"
-				>
-					Create Kanban Board
-				</Link>
+				<CreateBoardButton />
 			</section>
 			<About />
 			<Footer />
