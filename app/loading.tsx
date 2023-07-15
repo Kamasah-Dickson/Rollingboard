c4772d6c7942ccf5d3e23a13bf8193fa698c5ab0
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-15 04:19:25 +0000

    install react spinner and add loading.tsx to app route

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
-import Spinner from 'react-spinner';
-import 'react-spinner/react-spinner.css';
+"use client";
+
+import Spinner from "react-spinner";
+import "react-spinner/react-spinner.css";
 
 export default function Loading() {
 	// Or a custom loading skeleton component
-	return <Spinner/>;
+	return <Spinner />;
 }
