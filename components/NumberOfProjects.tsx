5565253e847c3447d48553890fc2605d95973037
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-18 16:31:50 +0000

    update number of projects

--- components/NumberOfProjects.tsx
+++ components/NumberOfProjects.tsx
@@ -12,14 +12,14 @@ const NumberOfProjects = () => {
 			<h1 className="text-xl md:text-2xl">{`Projects(${
 				Object.values(projectData)?.length
 			})`}</h1>
-			<div className="flex border border-white max-w-lg items-center p-1 justify-between gap-4">
+			{/* <div className="flex border border-white max-w-lg items-center p-1 justify-between gap-4">
 				<input
 					className="bg-transparent p-1 text-white  w-full text-xs outline-none"
 					type="text"
 					placeholder="Search Board..."
 				/>
 				<MdSearch color="white" size={25} />
-			</div>
+			</div> */}
 		</div>
 	);
 };
