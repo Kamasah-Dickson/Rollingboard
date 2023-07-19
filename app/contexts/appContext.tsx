fcdc3761bab934b2d244b6167131bdf995dc0248
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-19 16:45:00 +0000

    update context

--- app/contexts/appContext.tsx
+++ app/contexts/appContext.tsx
@@ -6,45 +6,30 @@ import {
 	Dispatch,
 	SetStateAction,
 	useState,
-	useEffect,
 } from "react";
-import { onValue, ref } from "firebase/database";
-import { auth, database } from "@/configs/firebase";
-import { Iproject } from "@/interface/interface";
 
 interface Imodal {
 	showModal: boolean;
 	modalType: string;
-	projectData: Iproject[];
+	userAuth: string;
 	setShowModal: Dispatch<SetStateAction<boolean>>;
 	setmodalType: Dispatch<SetStateAction<string>>;
-	setProjectData: Dispatch<SetStateAction<Iproject[]>>;
+	setUserAuth: Dispatch<SetStateAction<string>>;
 }
 
 export const modalContext = createContext<Imodal>({
 	showModal: false,
 	modalType: "",
-	projectData: [],
 	setShowModal: () => {},
 	setmodalType: () => {},
-	setProjectData: () => {},
+	setUserAuth: () => {},
+	userAuth: "",
 });
 
 const AppContext = ({ children }: { children: ReactElement }) => {
 	const [showModal, setShowModal] = useState(false);
 	const [modalType, setmodalType] = useState("");
-	const [projectData, setProjectData] = useState<Iproject[]>([]);
-
-	//fetch the data from firebase here and pass it as props to the project component
-	useEffect(() => {
-		const dbRef = ref(database, "projects/" + auth?.currentUser?.uid);
-		onValue(dbRef, (snapshot) => {
-			const data = snapshot.val();
-			if (data) {
-				setProjectData(data);
-			}
-		});
-	}, []);
+	const [userAuth, setUserAuth] = useState("");
 
 	return (
 		<modalContext.Provider
@@ -52,9 +37,9 @@ const AppContext = ({ children }: { children: ReactElement }) => {
 				showModal,
 				setShowModal,
 				modalType,
+				userAuth,
+				setUserAuth,
 				setmodalType,
-				projectData,
-				setProjectData,
 			}}
 		>
 			{children}
