0c54cf69aef17008292a20ad3426075831fe6a08
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-20 10:38:15 +0000

    update projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -28,24 +28,35 @@ const Projects = () => {
 		color: "",
 	});
 
-	const { showModal, setShowModal, setmodalType, modalType, userAuth } =
-		useContext(modalContext);
+	const {
+		showModal,
+		setShowModal,
+		setmodalType,
+		modalType,
+		userAuth,
+		setUserAuth,
+	} = useContext(modalContext);
 
 	const [projectData, setProjectData] = useState<Iproject[]>([]);
-
 	//fetch the data from firebase here and pass it as props to the project component
+	if (auth.currentUser?.uid) {
+		setUserAuth(auth.currentUser.uid);
+	}
 	useEffect(() => {
-		const dbRef = ref(database, "projects/" + userAuth);
-		onValue(dbRef, (snapshot) => {
-			const data = snapshot.val();
-			if (data) {
-				setProjectData(Object.values(data));
-			}
-		});
-	}, [userAuth]);
+		if (userAuth) {
+			const dbRef = ref(database, "projects/" + userAuth);
+			onValue(dbRef, (snapshot) => {
+				const data = snapshot.val();
+				if (data) {
+					setProjectData(data);
+					console.log(data);
+				}
+			});
+		}
+	}, [userAuth, setUserAuth]);
 
-	const handleClose = (id: string) => {
-		setProjectID(id);
+	const handleClose = (key: string) => {
+		setProjectID(key);
 		setmodalType("alert");
 		setShowModal(true);
 	};
@@ -85,15 +96,14 @@ const Projects = () => {
 		setProjectdefaultValues(project);
 	};
 
-	const totalProjects = projectData.reduce(
-		(accumulator, project) => accumulator + Object.values(project).length,
-		0
-	);
+	// console.log(projectData);
 
 	return (
 		<>
 			<div className="flex items-center max-w-lg mx-auto sm:max-w-full justify-between flex-col sm:flex-row gap-5">
-				<h1 className="text-xl md:text-2xl">{`Projects(${totalProjects})`}</h1>
+				<h1 className="text-xl md:text-2xl">{`Projects(${
+					Object.values(projectData).length
+				})`}</h1>
 				{/* <div className="flex border border-white max-w-lg items-center p-1 justify-between gap-4">
 				<input
 					className="bg-transparent p-1 text-white  w-full text-xs outline-none"
@@ -104,56 +114,54 @@ const Projects = () => {
 			</div> */}
 			</div>
 			<div className="mt-20 gap-5 my-grid">
-				{Object.entries(projectData).map(([key, projects]) => {
-					return Object.entries(projects).map(([key, project]) => {
-						return (
-							<div
-								key={key}
-								className="projectCard max-w-xl"
-								style={randomColor(project.color)}
-							>
-								<div className="before" style={border(project.color)}></div>
+				{Object.entries(projectData).map(([key, project]) => {
+					return (
+						<div
+							key={key}
+							className="projectCard max-w-xl"
+							style={randomColor(project.color)}
+						>
+							<div className="before" style={border(project.color)}></div>
 
-								<div className="card-inner">
-									<MdClose
-										size={25}
-										color="white"
-										cursor={"pointer"}
-										className=" select-none active:scale-[1.03] relative right-2 ml-auto max-w-fit top-2"
-										onClick={() => handleClose(key)}
-									/>
-									<div className=" mx-auto flex w-full flex-col items-center justify-center rounded-[17px] px-5 pt-16 ">
-										<h3 className="my-2 text-center text-lg font-medium md:text-xl">
-											{project?.projectName?.length > 30
-												? project.projectName.slice(0, 40) + "..."
-												: project.projectName}
-										</h3>
-										<p className="text-center text-sm font-normal text-[#909296]">
-											{project?.description?.length > 50
-												? project.description.slice(0, 100) + "..."
-												: project.description}
-										</p>
-									</div>
-									<div className="flex my-7 items-center gap-5 justify-center">
-										<Link
-											href={`/rollingboard/${key}`}
-											type="button"
-											className="active:scale-[1.03] text-white bg-[#47454580] p-2 text-sm rounded-sm"
-										>
-											Open board
-										</Link>
-										<button
-											onClick={() => handleProjectEdit(key, project)}
-											type="button"
-											className="text-white active:scale-[1.03] bg-[#47454580] p-2 text-sm rounded-sm"
-										>
-											Edit
-										</button>
-									</div>
+							<div className="card-inner">
+								<MdClose
+									size={25}
+									color="white"
+									cursor={"pointer"}
+									className=" select-none active:scale-[1.03] relative right-2 ml-auto max-w-fit top-2"
+									onClick={() => handleClose(key)}
+								/>
+								<div className=" mx-auto flex w-full flex-col items-center justify-center rounded-[17px] px-5 pt-16 ">
+									<h3 className="my-2 text-center text-lg font-medium md:text-xl">
+										{project?.projectName?.length > 30
+											? project.projectName.slice(0, 40) + "..."
+											: project.projectName}
+									</h3>
+									<p className="text-center text-sm font-normal text-[#909296]">
+										{project?.description?.length > 50
+											? project.description.slice(0, 100) + "..."
+											: project.description}
+									</p>
+								</div>
+								<div className="flex my-7 items-center gap-5 justify-center">
+									<Link
+										href={`/rollingboard/${key}`}
+										type="button"
+										className="active:scale-[1.03] text-white bg-[#47454580] p-2 text-sm rounded-sm"
+									>
+										Open board
+									</Link>
+									<button
+										onClick={() => handleProjectEdit(key, project)}
+										type="button"
+										className="text-white active:scale-[1.03] bg-[#47454580] p-2 text-sm rounded-sm"
+									>
+										Edit
+									</button>
 								</div>
 							</div>
-						);
-					});
+						</div>
+					);
 				})}
 
 				<div className="projectCard max-w-xl" style={randomColor("#06321899")}>
