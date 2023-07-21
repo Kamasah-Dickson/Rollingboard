da2597677b2fb2c12397500d38de2456bd2aae5b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-21 15:50:39 +0000

    update dynamic project route

--- app/rollingboard/[project]/page.tsx
+++ app/rollingboard/[project]/page.tsx
@@ -1,57 +1,22 @@
-"use client";
-
 import Footer from "@/components/Footer";
-import { auth, database } from "@/configs/firebase";
-import { Iproject } from "@/interface/interface";
-import { onValue } from "firebase/database";
-import { ref } from "firebase/database";
-import { useEffect, useState } from "react";
-import { IoIosArrowBack } from "react-icons/io";
-import Link from "next/link";
+import AppContext from "@/app/contexts/appContext";
+import Tasks from "@/components/Tasks";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Rollingboard - Tasks",
+};
 
 const SingleProject = ({ params }: { params: { project: string } }) => {
-	const [currentProject, setCurrentProject] = useState<Iproject[]>([
-		{
-			uid: "",
-			projectName: "",
-			description: "",
-			tasks: [{ backlog: [], todo: [], finished: [] }],
-			color: "",
-		},
-	]);
-
-	useEffect(() => {
-		const dbRef = ref(
-			database,
-			"projects/" + auth?.currentUser?.uid + `/${params.project}`
-		);
-
-		onValue(dbRef, (snapshot) => {
-			const data = snapshot.val();
-			if (data) {
-				setCurrentProject([data]);
-			}
-		});
-	}, [params.project]);
 	return (
-		<div className="h-screen flex flex-col">
-			<main className="mt-32 my-max flex-1">
-				{currentProject?.map((project) => {
-					return (
-						<div key={project.uid}>
-							<div className="flex items-center gap-5">
-								<Link href="/rollingboard">
-									<IoIosArrowBack color="white" cursor={"pointer"} size={30} />
-								</Link>
-								<h1 className="text-3xl md:text-4xl">{project.projectName}</h1>
-							</div>
-							<p className="ml-2 mt-3">{project.description}</p>
-						</div>
-					);
-				})}
-			</main>
+		<>
+			<AppContext>
+				<>
+					<Tasks params={params} />;
+				</>
+			</AppContext>
 			<Footer />
-		</div>
+		</>
 	);
 };
 
