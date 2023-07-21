aa089a58d1a4e583a85e1e99be6ba0865870c681
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-21 15:52:32 +0000

    update interface

--- interface/interface.ts
+++ interface/interface.ts
@@ -2,7 +2,11 @@ export interface Iproject {
 	uid: string;
 	projectName: string;
 	description: string;
-	tasks: { backlog: string[]; todo: string[]; finished: string[] }[];
+	// empty: boolean;
+	// backlog: string[];
+	// todo: string[];
+	// finished: string[];
+
 	color: string;
 }
 [];
