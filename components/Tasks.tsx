2a566817ab9f7af904b516bb2b96aa647778ed83
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-24 18:19:03 +0000

    feat:allow the update of project name

--- components/Tasks.tsx
+++ components/Tasks.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 import { modalContext } from "@/app/contexts/appContext";
 import { auth, database } from "@/configs/firebase";
 import { Iproject, Itask } from "@/interface/interface";
-import { onValue, ref, set } from "firebase/database";
+import { onValue, ref, set, update } from "firebase/database";
 import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
@@ -19,8 +19,11 @@ const Tasks = ({ params }: { params: { project: string } }) => {
 	const searchParams = useSearchParams();
 	const searchParamsID = searchParams.get("uid");
 	const [isSending, setIsSending] = useState(false);
+	const [theEditingTaskID, setTheEditingTaskID] = useState<null | string>("");
+	const [editedTaskName, setEditedTaskName] = useState("");
 
 	const inputRef = useRef<HTMLInputElement>(null);
+	const inputRef2 = useRef<HTMLInputElement>(null);
 
 	const {
 		setShowModal,
@@ -49,12 +52,17 @@ const Tasks = ({ params }: { params: { project: string } }) => {
 		},
 	]);
 
-	// tasks: { id: string; taskTitle: ""; children: { task: "" } }[];
 	useEffect(() => {
 		if (showInput && inputRef.current) {
 			inputRef.current.focus();
 		}
 	}, [showInput]);
+
+	useEffect(() => {
+		if (theEditingTaskID && inputRef2.current) {
+			inputRef2.current.focus();
+		}
+	}, [theEditingTaskID]);
 
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -137,6 +145,52 @@ const Tasks = ({ params }: { params: { project: string } }) => {
 		setColumnName((prev) => ({ ...prev, column: columnName, uid }));
 	};
 
+	const toggleTaskEdit = (taskID: string, taskName: string) => {
+		setEditedTaskName(taskName);
+		setTheEditingTaskID(taskID === theEditingTaskID ? null : taskID);
+	};
+
+	const handleTaskNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setEditedTaskName(e.target.value);
+	};
+
+	const saveEditedTask = (taskID: string) => {
+		setTasks((prevTask) =>
+			prevTask.map((task) =>
+				task.uid === taskID ? { ...task, taskName: editedTaskName } : task
+			)
+		);
+		setTheEditingTaskID(null);
+	};
+
+	const handleTaskNameKeyDown = (
+		e: React.KeyboardEvent<HTMLInputElement>,
+		uid: string
+	) => {
+		if (e.key === "Enter") {
+			saveEditedTask(uid);
+
+			const updateRef = ref(
+				database,
+				"projects/" +
+					auth?.currentUser?.uid +
+					"/" +
+					searchParamsID +
+					"/tasks/ " +
+					uid
+			);
+
+			const updates = {
+				taskName: editedTaskName,
+			};
+
+			update(updateRef, updates);
+			toast.success(`Project has been updated`, {
+				position: toast.POSITION.TOP_CENTER,
+			});
+		}
+	};
+
 	return (
 		<div className="h-screen flex flex-col">
 			<main className="mt-32 my-max flex-1">
@@ -168,14 +222,32 @@ const Tasks = ({ params }: { params: { project: string } }) => {
 											className="flex max-w-[300px] w-full justify-center flex-col rounded-md bg-[#101113] border border-[#8080802f] p-4 text-white"
 										>
 											<span className="mb-2 flex items-center justify-between">
-												<h2
-													title={task.taskName}
-													className="capitalize cursor-pointer text-xl font-medium "
-												>
-													{task.taskName.length > 20
-														? task.taskName.slice(0, 20) + "..."
-														: task.taskName}
-												</h2>
+												{theEditingTaskID === task.uid ? (
+													<input
+														ref={inputRef2}
+														type="text"
+														value={editedTaskName}
+														onChange={(e) => handleTaskNameChange(e)}
+														placeholder="Enter task name"
+														onBlur={() => setTheEditingTaskID(null)}
+														className="text-white bg-transparent border p-1 outline-none border-[#80808041]"
+														onKeyDown={(e) =>
+															handleTaskNameKeyDown(e, task.uid)
+														}
+													/>
+												) : (
+													<h2
+														onClick={() =>
+															toggleTaskEdit(task.uid, task.taskName)
+														}
+														title={task.taskName}
+														className="capitalize cursor-pointer text-xl font-medium "
+													>
+														{task.taskName.length > 20
+															? task.taskName.slice(0, 20) + "..."
+															: task.taskName}
+													</h2>
+												)}
 												<MdClose
 													onClick={() =>
 														handleTaskRemove(task.uid, task.taskName)
@@ -208,7 +280,6 @@ const Tasks = ({ params }: { params: { project: string } }) => {
 									) : (
 										<button
 											onClick={() => setShowInput(true)}
-											// onClick={handleNewTask}
 											className="text-white transition-colors hover:border-white active:scale-[1.02] border border-[#ffffff5b] px-7 py-2 rounded-lg text-xl flex items-center gap-3"
 										>
 											<AiOutlinePlus color="white" size={23} />
