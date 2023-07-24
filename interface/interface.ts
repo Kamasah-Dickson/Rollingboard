e4155cdb97ce639eb94c7f70e55da872c1af31d2
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-07-24 13:40:54 +0000

    update interface

--- interface/interface.ts
+++ interface/interface.ts
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from "react";
 export interface Iproject {
 	uid: string;
 	projectName: string;
@@ -10,3 +11,39 @@ export interface Iproject {
 	color: string;
 }
 [];
+
+export interface IDefault {
+	uid: string;
+	projectName: string;
+	description: string;
+	color: string;
+}
+
+export interface Itask {
+	uid: string;
+	taskName: string;
+}
+
+export interface Imodal {
+	showModal: boolean;
+	modalType: string;
+	tasks: Itask[];
+	columnName: {
+		column: string;
+		uid: string;
+	};
+	setShowModal: Dispatch<SetStateAction<boolean>>;
+	setmodalType: Dispatch<SetStateAction<string>>;
+	setTasks: Dispatch<SetStateAction<Itask[]>>;
+	setColumnName: Dispatch<
+		SetStateAction<{
+			column: string;
+			uid: string;
+		}>
+	>;
+}
+
+export interface Icolumn {
+	column: string;
+	uid: string;
+}
